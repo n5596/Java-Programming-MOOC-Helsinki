7502c64a6bcfb65ba3b556c989064a3201dc1a8c
Naila <nfatima5596@gmail.com>
2022-07-03 04:39:04 -0400

    Add files via upload

--- Java-Programming-II/Part 13/Notifier/NotifierApplication.java
+++ Java-Programming-II/Part 13/Notifier/NotifierApplication.java
@@ -5,11 +5,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
-import javafx.event.EventHandler;
-import javafx.event.ActionEvent;
 
 
 public class NotifierApplication extends Application{
